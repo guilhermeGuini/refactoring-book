f07c743bfca6f3ea9470795856fc01dda58fd428
Guilherme Silva Guini <gguini@alelo.com.br>
2022-05-05 11:42:29 -0300

    Moving constant to new method

--- core.js
+++ core.js
@@ -2,9 +2,11 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-                             { style: "currency", currency: "USD",
-                               minimumFractionDigits: 2 }).format;
+
+    function format(amount) {
+        return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD",
+                                                minimumFractionDigits: 2 }).format(amount);
+    }
 
     function calcAmount(type, audience) {
         let amountResponse;
