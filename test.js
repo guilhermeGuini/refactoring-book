453521e1eb076c816942a045b2de09cbe1f9f13c
Guilherme Silva Guini <gguini@alelo.com.br>
2022-05-05 18:12:55 -0300

    Moving tests to another file and add iterator to invoices

---
+++ test.js
@@ -0,0 +1,25 @@
+import * as business from './business.js';
+import { invoices } from './invoices.js';
+import { plays } from './plays.js';
+import { text } from './report.js';
+
+let data = {
+    plays: plays(),
+    customer: invoices()[0].customer,
+    performances: business.createNewObjectWithAmount(invoices()[0].performances, plays()),
+    totalAmount: business.totalAmount(invoices()[0].performances, plays()),
+    totalVolumeCredits: business.totalVolumeCredits(invoices()[0].performances, plays())
+}
+
+let result = text(data);
+console.log(result)
+
+let arrResult = result.split('\n');
+
+console.assert(arrResult[0] === 'Statement for BigCo');
+console.assert(arrResult[1] === ' Hamlet: $650.00 (55 seats)');
+console.assert(arrResult[2] === ' As You Like It: $580.00 (35 seats)');
+console.assert(arrResult[3] === ' Othello: $500.00 (40 seats)');
+console.assert(arrResult[4] === 'Amount owed is $1,730.00');
+console.assert(arrResult[5] === 'You earned 47 credits');
+console.assert(arrResult[6] === '');
