0e8029cd21669e4a0de2f841f9ea056df0656434
Guilherme Silva Guini <gguini@alelo.com.br>
2022-05-06 09:37:14 -0300

    Refactoring tests to use mocha chai

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ import { invoices } from './invoices.js';
 import { plays } from './plays.js';
 import { text } from './report.js';
 
+import { expect } from 'chai';
+
 let data = {
     plays: plays(),
     customer: invoices()[0].customer,
@@ -13,13 +15,24 @@ let data = {
 
 let result = text(data);
 console.log(result)
-
 let arrResult = result.split('\n');
 
-console.assert(arrResult[0] === 'Statement for BigCo');
-console.assert(arrResult[1] === ' Hamlet: $650.00 (55 seats)');
-console.assert(arrResult[2] === ' As You Like It: $580.00 (35 seats)');
-console.assert(arrResult[3] === ' Othello: $500.00 (40 seats)');
-console.assert(arrResult[4] === 'Amount owed is $1,730.00');
-console.assert(arrResult[5] === 'You earned 47 credits');
-console.assert(arrResult[6] === '');
+describe('text', function() {
+    it('customerName', function() {
+        expect(arrResult[0]).equal('Statement for BigCo');
+    });
+
+    it('plays', function() {
+        expect(arrResult[1]).equal(' Hamlet: $650.00 (55 seats)');
+        expect(arrResult[2]).equal(' As You Like It: $580.00 (35 seats)');
+        expect(arrResult[3]).equal(' Othello: $500.00 (40 seats)');
+    });
+
+    it('totalAmount', function() {
+        expect(arrResult[4]).equal('Amount owed is $1,730.00');
+    });
+
+    it ('totalCredits', function() {
+        expect(arrResult[5]).equal('You earned 47 credits');
+    });
+});
